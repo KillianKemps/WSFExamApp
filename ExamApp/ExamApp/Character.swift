eadb69ce7ef4cc26f20601ece009461a3053816f
KillianKemps <killian.kemps@etu-webschoolfactory.fr>
2016-06-10 11:02:42 +0200

    Install Podfile dependencies

--- ExamApp/ExamApp/Character.swift
+++ ExamApp/ExamApp/Character.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Alamofire
 
 struct Character {
     
@@ -23,5 +24,21 @@ struct Character {
         birth_year = dict["birth_year"] as! String
         height = dict["height"] as! Int
         mass = dict["mass"] as! Int
+    }
+}
+
+extension Character {
+    static func getRemoteComics(offset: Int, completionHandler: Response<AnyObject, NSError> -> Void) {
+        let apiKey = "c3901f9b2fb11e2322853b3ede27f438"
+        let ts = "testtest"
+        let hash = "1d312b3f93ad2dbbd2ab9af0a125b73a"
+        
+        let param: [String : AnyObject] = ["apikey" : apiKey, "hash" : hash, "ts" : ts, "offset": offset]
+        
+        
+        Alamofire.request(.GET, "http://gateway.marvel.com/v1/public/comics", parameters: param).responseJSON { response in
+            
+            completionHandler(response)
+        }
     }
 }
